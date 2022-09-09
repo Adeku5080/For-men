0d98848855a5d745cdf70d91de4b7cd08cea0133
Adeku Ali <Aliadeku.aam@gmail.com>
2022-09-09 22:59:33 +0100

    create checkout page

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -2,10 +2,33 @@
 <html>
 <head>
     <title>
-         checkout
+        checkout
     </title>
+    <link rel="stylesheet" href="{{ asset('vendor/css/checkout.css') }}">
 </head>
 <body>
-  <h3>checkout page</h3>
+<h3>Secure Checkout</h3>
+
+<Section class="checkout-section">
+    <h4 class="section-title">shipping address</h4>
+    <div class="checkout-form-section">
+        <form>
+            <input name="firstname" type="text" placeholder="firstname"/>
+            <input name="lastname" type="text" placeholder="lastname"/>
+            <input name="email" type="email" placeholder="Email"/>
+            <input name="address" placeholder="Address 1"/>
+            <input name="phone" placeholder="Phone"/>
+
+            <select name="country" id="country">
+
+
+            </select>
+
+
+            <button class="checkout-btn" type="button">continue checkout</button>
+        </form>
+    </div>
+
+</Section>
 </body>
 </html>
